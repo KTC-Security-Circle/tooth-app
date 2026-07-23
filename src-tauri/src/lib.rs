9fab104355e1aa19c8a7cb1f8640a2fe952f9981
haruki26 <h826.a519@gmail.com>
2026-07-24 00:44:09 +0900

    chore: toolsのビルド設定

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -37,6 +37,7 @@ pub fn run() {
         )
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             commands::greet::greet,
             commands::camera::list_cameras,
